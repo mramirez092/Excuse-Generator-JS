afd726c4856684d3f40e6d3a9f86810ef8af33a3
mramirez092 <miguelramirez2210@gmail.com>
2023-03-07 18:52:17 +0000

    Generador terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+  console.log("Hello World");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoNum = Math.floor(Math.random() * who.length);
+  let whatNum = Math.floor(Math.random() * what.length);
+  let whenNum = Math.floor(Math.random() * when.length);
+
+  return who[whoNum] + " " + what[whatNum] + " " + when[whenNum] + " ";
 };
